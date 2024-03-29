8268a314da23fc119e2a862e1afca913a782ce4d
Sabirov Daniyar <sabirov.astana@gmail.com>
2024-03-29 17:58:33 +0100

    init backend

---
+++ backend/src/main/java/cz/cvut/fit/sabirdan/wework/WeWorkApplication.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.sabirdan.wework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeWorkApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/cz/cvut/fit/sabirdan/wework/WeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.sabirdan.wework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
