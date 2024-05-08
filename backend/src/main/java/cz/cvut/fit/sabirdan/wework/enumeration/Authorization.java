9d784b1084350ecb3996cfdc4e7e7a7c792a8dca
Daniyar Sabirov <sabirdan@cvut.cz>
2024-05-08 16:24:12 +0200

    feat: populate authorizations

--- backend/src/main/java/cz/cvut/fit/sabirdan/wework/enumeration/Authorization.java
+++ backend/src/main/java/cz/cvut/fit/sabirdan/wework/enumeration/Authorization.java
@@ -2,6 +2,10 @@ package cz.cvut.fit.sabirdan.wework.enumeration;
 
 public enum Authorization {
     // TODO
-    READ,
-    WRITE
+    SYSTEM_USER_BASIC,
+    SYSTEM_ADMIN_BASIC,
+    SYSTEM_READ_USERS,
+    SYSTEM_READ_PROJECTS,
+    SYSTEM_READ_TASKS,
+    SYSTEM_READ_MEMBERS,
 }
