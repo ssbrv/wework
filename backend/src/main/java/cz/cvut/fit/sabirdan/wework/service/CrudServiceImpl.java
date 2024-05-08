bfd57b5c7028975ff95855489b176c79530653f4
Daniyar Sabirov <sabirdan@cvut.cz>
2024-05-08 16:26:18 +0200

    fix: get read of "already exists" situation, because it never happens, because database handles ids itself

--- backend/src/main/java/cz/cvut/fit/sabirdan/wework/service/CrudServiceImpl.java
+++ backend/src/main/java/cz/cvut/fit/sabirdan/wework/service/CrudServiceImpl.java
@@ -1,27 +1,26 @@
 package cz.cvut.fit.sabirdan.wework.service;
 
 import cz.cvut.fit.sabirdan.wework.domain.EntityWithIdLong;
+import cz.cvut.fit.sabirdan.wework.utilites.exception.BadRequestException;
 import cz.cvut.fit.sabirdan.wework.utilites.exception.ConflictException;
 import cz.cvut.fit.sabirdan.wework.utilites.exception.NotFoundException;
+import io.micrometer.common.util.StringUtils;
 
 import java.util.Optional;
 
 public abstract class CrudServiceImpl<T extends EntityWithIdLong> implements CrudService<T> {
     @Override
-    public T create(T e) {
-        if (e.getId() != null && getRepository().existsById(e.getId()))
-            throw new ConflictException(getEntityName() + " with such id already exists");
-
+    public T save(T e) {
         return getRepository().save(e);
     }
 
     @Override
-    public Optional<T> readById(Long id) {
+    public Optional<T> findById(Long id) {
         return getRepository().findById(id);
     }
 
     @Override
-    public Iterable<T> readAll() {
+    public Iterable<T> findAll() {
         return getRepository().findAll();
     }
 
@@ -33,7 +32,7 @@ public abstract class CrudServiceImpl<T extends EntityWithIdLong> implements Cru
     @Override
     public void deleteById(Long id){
         if (!getRepository().existsById(id))
-            throw new NotFoundException(getEntityName() + " with such id does not exist");
+            throw new NotFoundException("This " + getEntityName().toLowerCase() + " does not exist. Probably somebody already deleted the " + getEntityName().toLowerCase());
 
         getRepository().deleteById(id);
     }
