6b62b39110aeb9c744f2fa681f9300e1368c8bb7
Daniyar Sabirov <sabirdan@cvut.cz>
2024-05-08 16:25:01 +0200

    fix: match service method namings with repository method namings

--- backend/src/main/java/cz/cvut/fit/sabirdan/wework/service/CrudService.java
+++ backend/src/main/java/cz/cvut/fit/sabirdan/wework/service/CrudService.java
@@ -5,9 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CrudService<T> {
-    T create(T e);
-    Optional<T> readById(Long id);
-    Iterable<T> readAll();
+    T save(T e);
+    Optional<T> findById(Long id);
+    Iterable<T> findAll();
     void update(Long id, T e);
     void deleteById(Long id);
     JpaRepository<T, Long> getRepository();
