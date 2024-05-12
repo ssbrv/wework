e5546f074321db62088cc2d66188b78b5b75b7a9
Daniyar Sabirov <sabirdan@cvut.cz>
2024-05-12 14:45:57 +0200

    feat: logo

--- frontend/src/components/WeWorkLogo/WeWorkLogo.tsx
+++ frontend/src/components/WeWorkLogo/WeWorkLogo.tsx
@@ -1,13 +1,23 @@
 import { Image } from "@mantine/core";
 
-export const WeWorkLogo = (): JSX.Element => {
+interface Props {
+  withImage?: boolean;
+  withName?: boolean;
+}
+
+export const WeWorkLogo = ({
+  withImage = true,
+  withName = true,
+}: Props): JSX.Element => {
   return (
     <div className="flex items-end justify-center gap-2">
-      <Image src="wework.png" className="size-24" />
-      <div className="font-bold text-2xl">
-        we <br />
-        work.
-      </div>
+      {withImage && <Image src="wework.png" className="size-24" />}
+      {withName && (
+        <div className="font-bold text-2xl">
+          we <br />
+          work.
+        </div>
+      )}
     </div>
   );
 };
