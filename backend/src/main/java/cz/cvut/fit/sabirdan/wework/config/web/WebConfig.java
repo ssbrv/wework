8275bb36b51dfa7d71eb1e1a5cfa58d322ba414a
Daniyar Sabirov <sabirdan@cvut.cz>
2024-05-12 14:42:15 +0200

    fix: cross origin

--- backend/src/main/java/cz/cvut/fit/sabirdan/wework/config/web/WebConfig.java
+++ backend/src/main/java/cz/cvut/fit/sabirdan/wework/config/web/WebConfig.java
@@ -1,6 +1,7 @@
 package cz.cvut.fit.sabirdan.wework.config.web;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -19,5 +20,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("forward:/index.html");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
--- backend/src/main/java/cz/cvut/fit/sabirdan/wework/controller/user/UserController.java
+++ backend/src/main/java/cz/cvut/fit/sabirdan/wework/controller/user/UserController.java
@@ -8,9 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:3000")
+
 @RestController
 @RequestMapping("/api/v1/user")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
     private final UserService userService;
