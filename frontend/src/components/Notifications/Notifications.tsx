f47bc9a45553b1b0fc5b8f1ac3f6a7535be7cd93
Daniyar Sabirov <sabirdan@cvut.cz>
2024-05-12 14:45:48 +0200

    feat: add standard notifications

---
+++ frontend/src/components/Notifications/Notifications.tsx
@@ -0,0 +1,44 @@
+import { notifications } from '@mantine/notifications';
+
+// autoclose, green background
+export const goodNotification = (
+  notificationTitle?: string,
+  notificationMessage?: string
+) => {
+  notifications.show({
+    color: 'green',
+    title: notificationTitle,
+    message: notificationMessage,
+    radius: 'md',
+    autoClose: 4000,
+    style: { backgroundColor: '#d9ffde' },
+  });
+};
+
+// autoclose, white background
+export const okNotification = (
+  notificationTitle?: string,
+  notificationMessage?: string
+) => {
+  notifications.show({
+    title: notificationTitle,
+    message: notificationMessage,
+    radius: 'md',
+    autoClose: 4000,
+  });
+};
+
+// no autoclose, pink background
+export const badNotification = (
+  notificationTitle?: string,
+  notificationMessage?: string
+) => {
+  notifications.show({
+    color: 'red',
+    title: notificationTitle,
+    message: notificationMessage,
+    radius: 'md',
+    style: { backgroundColor: '#fff1f0' },
+    autoClose: false,
+  });
+};
