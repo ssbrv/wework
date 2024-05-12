9faafb90b075f70c9489cbf1d4d98c67f55988c2
Daniyar Sabirov <sabirdan@cvut.cz>
2024-05-12 21:05:13 +0200

    fix: reduce code by using PreAuth component

--- frontend/src/pages/NotFound/NotFoundPage.tsx
+++ frontend/src/pages/NotFound/NotFoundPage.tsx
@@ -1,26 +1,21 @@
-import { Button, Stack, Title } from "@mantine/core";
-import { WeWorkLogo } from "../../components/WeWorkLogo/WeWorkLogo";
+import { Button, Title } from "@mantine/core";
 import { MoodSad } from "tabler-icons-react";
+import PreAuth from "../../components/PreAuth/PreAuth";
 
 const NotFoundPage = (): JSX.Element => {
   return (
-    <div className="min-h-screen flex p-4 justify-center items-center bg-gradient-to-br from-gradient-from to-gradient-to">
-      <div className="max-w-[30rem] w-full bg-secondary card-lg p-8 flex flex-col gap-8">
-        <Stack className="gap-6">
-          <WeWorkLogo />
-          <div className="flex items-center w-full gap-4">
-            <MoodSad className="size-12 text-danger" />
-            <Title order={2}>The page was not found...</Title>
-          </div>
-          <Button radius="md" size="md" component="a" href="login">
-            Go to login
-          </Button>
-          <Button radius="md" size="md" component="a" href="profile">
-            Go to my profile
-          </Button>
-        </Stack>
+    <PreAuth>
+      <div className="flex items-center w-full gap-4">
+        <MoodSad className="size-12 text-danger" />
+        <Title order={2}>The page was not found...</Title>
       </div>
-    </div>
+      <Button radius="md" size="md" component="a" href="login">
+        Go to login
+      </Button>
+      <Button radius="md" size="md" component="a" href="profile">
+        Go to my profile
+      </Button>
+    </PreAuth>
   );
 };
 
