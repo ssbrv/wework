6631cb7c0cea276d1e24e4dbf4862a28bf7d0ae3
Daniyar Sabirov <sabirdan@cvut.cz>
2024-07-23 21:41:33 +0200

    remove junk comments

--- frontend/src/utils/utils.tsx
+++ frontend/src/utils/utils.tsx
@@ -1,10 +1,9 @@
 export function formatDate(date: Date): string {
   const options: Intl.DateTimeFormatOptions = {
-    month: "long", // Full month name (e.g., "January")
-    day: "numeric", // Numeric day of the month (e.g., "1")
-    year: "numeric", // Full numeric year (e.g., "2024")
+    month: "long",
+    day: "numeric",
+    year: "numeric",
   };
 
-  // Use toLocaleDateString to format the date according to options
   return date.toLocaleDateString("en-US", options);
 }
